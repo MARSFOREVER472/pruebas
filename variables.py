f5e0b6edbd2efed3b605175556e7186cc261abaf
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-08-30 14:24:04 -0400

    Variables en Python ya creadas desde cero

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Algunas variables a declarar a continuación:
+
+a = 1 # Le daremos por ejemplo el valor 1 a la letra 'a'
+b = 2 # Le daremos por ejemplo el valor 2 a la letra 'b'
+c = 3 # Le daremos por ejemplo el valor 3 a la letra 'c'
+d = 4 # Le daremos por ejemplo el valor 4 a la letra 'd'
+e = 5 # Le daremos por ejemplo el valor 5 a la letra 'e'
+f = 6 # Le daremos por ejemplo el valor 6 a la letra 'f'
+g = 7 # Le daremos por ejemplo el valor 7 a la letra 'g'
+h = 8 # Le daremos por ejemplo el valor 8 a la letra 'h'
+i = 9 # Le daremos por ejemplo el valor 9 a la letra 'i'
+j = 10 # Le daremos por ejemplo el valor 10 a la letra 'j'
+
+# Realizaremos una operatoria con una suma para la letra k.
+
+k = a + b + c + d + e + f + g + h + i + j
+
+print(k) # El valor de 'k' será el resultado entregado por la operatoria con sumas de valores (De la 'a' a la 'j')
+
+# Ahora, lo que vamos a hacer, vendría un proceso más embestido a las variables.
+
+mi_texto = 'MENTIRA, PERRITO'
+
+print(str(mi_texto)) # Es un string que se entregó a la variable ya declarada al inicio de este proceso.
+
+
