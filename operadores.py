c88c6cbea0783577013924920fe693e696a01100
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-08-30 22:26:57 -0400

    Se aplicaron algunos operadores con números

---
+++ operadores.py
@@ -0,0 +1,121 @@
+# OPERADORES ARITMÉTICOS EN PYTHON.
+
+print(str('***********BIENVENIDO A ESTA SECCIÓN DE OPERADORES***********'))
+print(str('Vamos a repasar algunas operaciones matemáticas para aplicaciones posibles!'))
+
+print(str('Suma de 2 números'))
+print(6 + 2) # Suma de 2 números.
+
+print(str('Resta de 2 números'))
+print(6 - 2) # Resta de 2 números.
+
+print(str('Multiplicación de 2 números'))
+print(6 * 2) # Multiplicación de 2 números.
+
+print(str('División de 2 números entregado en decimales'))
+print(6 / 2) # División de 2 números en decimal.
+
+print(str('Esto es un operador de módulos analógicos'))
+print(2 % 6) # Operador de módulo.
+
+print(str('Potencia de un número base elevado a un número cualquiera'))
+print(6 ** 2) # Potencia de un número cualquiera.
+
+print(str('División de 2 números entregado en números enteros'))
+print(6 // 2) # División de 2 números en enteros.
+
+# Suponiendo un valor de 'a' que es igual a 3:
+
+a = 3
+print('Bien ' + 'Amigos' + '!')
+print('Aquí ' * a) # Hay que tener en cuenta que para validar este proceso, se tiene que multiplicar la sentencia principal por 3.
+
+# OPERADORES COMPARATIVOS EN PYTHON.
+
+print(str('Vamos a verificar si es que cada una de estas aseveraciones se cumple o no de la siguiente manera:'))
+
+print(str('Ejercicio 1: 4 < 8'))
+print(4 < 8) # Si 4 es menor que 8.
+
+print(str('Ejercicio 2: 4 > 8'))
+print(4 > 8) # Si 4 es mayor que 8.
+print(str('Respuesta: 4 tiene que ser menor que 8'))
+
+print(str('Ejercicio 3: 4 = 8'))
+print(4 == 8) # Si 4 es igual que 8.
+print(str('Respuesta: 4 tiene que ser igual que 4'))
+
+print(str('Ejercicio 4: 4 <= 8'))
+print(4 <= 8) # Si 4 es menor o igual que 8.
+
+print(str('Ejercicio 5: 4 >= 8'))
+print(4 >= 8) # Si 4 es mayor o igual que 8.
+print(str('Respuesta: 4 tiene que ser menor o igual que 8'))
+
+print(str('Ejercicio 6: 4 != 8'))
+print(4 != 8) # Si 4 es distinto que 8.
+
+# Lo vamos a aplicar con incógnitas, suponiendo que:
+
+a = 3
+b = 6
+c = 9
+
+# Algunas incógnitas adicionales para verificar al 100%...
+
+d = 3
+e = 15
+f = 6
+
+print(str('Con estas funciones declaradas anteriormente, podemos aplicarlo de la misma forma:'))
+
+print(str('Ejercicio 1.1.- a > b'))
+print(a > b)
+
+print(str('Ejercicio 1.2.- b > c'))
+print(b > c)
+
+print(str('Ejercicio 1.3.- a > c'))
+print(a > c)
+
+print(str('Ejercicio 2.1.- a < b'))
+print(a < b)
+
+print(str('Ejercicio 2.2.- b < c'))
+print(b < c)
+
+print(str('Ejercicio 2.3.- a < c'))
+print(a < c)
+
+print(str('Ejercicio 3.1.- a = b'))
+print(a == b)
+
+print(str('Ejercicio 3.2.- b = c'))
+print(b == c)
+
+print(str('Ejercicio 3.3.- a = c'))
+print(a == c)
+
+print(str('Ejercicio 3.4.- a = d'))
+print(a == d)
+
+print(str('Ejercicio 3.5.- b = f'))
+print(b == f)
+
+print(str('Ejercicio 3.6.- e > c'))
+print(e > c)
+
+# OPERADORES LÓGICOS EN PYTHON
+
+print(str('Finalmente, practiquemos con los operadores booleanos'))
+
+print(str('Cuando es verdadero y falso, según sea el caso'))
+print(True and False) # OPERADOR 'AND'
+
+print(str('Cuando es verdadero o falso, según sea el caso'))
+print(True or False) # OPERADOR 'OR'
+
+print(str('Mediante una respuesta, se indica aquí si la operación entregada es verdadera o falsa'))
+print(not(4 > 6)) # OPERADOR 'NOT'
+
+print(str('**********AQUÍ TERMINA ESTA SECCIÓN**********'))
